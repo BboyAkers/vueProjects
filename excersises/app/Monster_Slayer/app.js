1917b7c2adc0bc36df3091e07f710e173ce80fd9
Austin Akers <austin.akers5@gmail.com>
2017-07-01 14:11:31 -0500

    finished monster slayer game

--- excersises/app/Monster_Slayer/app.js
+++ excersises/app/Monster_Slayer/app.js
@@ -6,7 +6,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
 
     methods: {
@@ -15,26 +16,59 @@ new Vue({
            this.gameIsRunning = true;
            this.playerHealth = 100;
            this.monsterHealth = 100;
+           this.turns = [];
        },
+        monsterAttacks: function () {
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            });
+        },
 
         calculateDamage: function (min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
         },
         attack: function () {
-
-            this.monsterHealth -= this.calculateDamage(3, 10);
-
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
             if(this.checkWin()) {
                 return;
             }
-            this.playerHealth -= this.calculateDamage(5, 12);
-            this.checkWin();
+            this.monsterAttacks();
         },
         specialAttack: function () {
-            
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            });
+            if(this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
+
         },
         heal: function () {
-            
+            if(this.playerHealth <= 90) {
+                this.playerHealth += 10;
+                this.monsterAttacks();
+            }
+            else {
+                this.playerHealth = 100;
+            }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
+            this.monsterAttacks();
         },
         giveUp: function () {
             
