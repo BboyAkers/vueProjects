e83c77c4f2c9c22191c6b3a9848a95797faaa46d
Austin Akers <austin.akers5@gmail.com>
2017-07-01 13:09:45 -0500

    created basic atttack logic and end game logic

---
+++ excersises/app/Monster_Slayer/app.js
@@ -0,0 +1,51 @@
+/**
+ * Created by austi on 7/1/2017.
+ */
+new Vue({
+   el: '#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false
+    },
+
+    methods: {
+
+        startGame: function () {
+           this.gameIsRunning = true;
+           this.playerHealth = 100;
+           this.monsterHealth = 100;
+       },
+        attack: function () {
+            var max = 10;
+            var min = 3;
+            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.monsterHealth -= damage;
+
+            if(this.monsterHealth <= 0) {
+                alert('You won!');
+                this.gameIsRunning = false;
+                return;
+            }
+            if(this.playerHealth <= 0) {
+                alert('You lost!');
+                this.gameIsRunning = false;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.playerHealth -= damage
+        },
+        specialAttack: function () {
+            
+        },
+        heal: function () {
+            
+        },
+        giveUp: function () {
+            
+        }
+    }
+});
