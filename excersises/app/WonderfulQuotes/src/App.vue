c0ec4ec59d693f80a26cb49b3859930911224e43
Austin Akers <austin.akers5@gmail.com>
2017-07-16 10:04:27 -0500

    Passed quotes data through components

---
+++ excersises/app/WonderfulQuotes/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    export default {
+        components: {
+            appQuoteGrid: QuoteGrid
+        },
+        data: function () {
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ excersises/app/WonderfulQuotes/src/components/QuoteGrid.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row">
+        <app-quote v-for="quote in quotes">{{quote}}</app-quote>
+    </div>
+</template>
+
+
+<style>
+
+</style>
+
+<script>
+    import Quote from './Quote.vue';
+    export default {
+        components: {
+          appQuote: Quote
+        },
+        props: ['quotes']
+    }
+
+</script>
