0667654a6a8cdbfbcf07566914cd7078f89bb8f9
Austin Akers <austin.akers5@gmail.com>
2017-07-22 10:06:19 -0500

    created form data and bindings

--- excersises/app/exercise9/src/App.vue
+++ excersises/app/exercise9/src/App.vue
@@ -5,10 +5,26 @@
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
+                    <label for="firstName"></label>
+                    <input type="text" id="firstName" class="form-control" v-model="userData.firstName"/>
+
+                    <label for="lastName"></label>
+                    <input type="text" id="lastName" class="form-control" v-model="userData.lastName"/>
                     <!-- Full Name (First Name + Last Name) -->
                     <!-- Mail -->
+                    <label for="mail"></label>
+                    <input type="text" id="mail" class="form-control" v-model="userData.mail"/>
                     <!-- Password -->
+                    <label for="password"></label>
+                    <input type="password" id="password" class="form-control" v-model="userData.password"/>
                     <!-- Store Data? Yes/No -->
+                    <p>Stored Data?</p>
+                    <label for="yes">
+                        <input type="radio" id="yes" value="Yes" v-model="storedData">Yes
+                    </label>
+                    <label for="no">
+                        <input type="radio" id="no" value="No" v-model="storedData">Yes
+                    </label>
 
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
@@ -41,6 +57,18 @@
 
 <script>
     export default {
+        data() {
+            return {
+                userData: {
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    password: '',
+
+                },
+                storedData: 'Yes'
+            }
+        }
     }
 </script>
 
