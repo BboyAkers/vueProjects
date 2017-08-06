7e4da5e5eda6de1b8273827a262ee71ad60cace7
Austin Akers <austin.akers5@gmail.com>
2017-08-05 23:24:53 -0500

    created child routes for User

--- excersises/app/routing/src/components/user/User.vue
+++ excersises/app/routing/src/components/user/User.vue
@@ -3,6 +3,8 @@
         <h1>The User Page</h1>
         <hr>
         <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+        <hr>
+        <router-view></router-view>
     </div>
 
 </template>
--- excersises/app/routing/src/routes.js
+++ excersises/app/routing/src/routes.js
@@ -1,5 +1,8 @@
 import Home from './components/Home.vue';
 import User from './components/user/User.vue';
+import UserStart from './components/user/UserStart.vue';
+import UserDetail from './components/user/UserDetail.vue';
+import UserEdit from './components/user/UserEdit.vue';
 
 
 export const routes = [
@@ -9,6 +12,20 @@ export const routes = [
     },
     {
         path: '/users',
-        component: User
+        component: User,
+        children: [
+            {
+                path: '',
+                component: UserStart
+            },
+            {
+                path: ':id',
+                component: UserDetail
+            },
+            {
+                path: ':id/edit',
+                component: UserEdit
+            }
+        ]
     },
 ];
