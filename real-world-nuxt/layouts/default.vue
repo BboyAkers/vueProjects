e7a5beceede97f37b02ae14e2f5e16027ee66e23
unknown <austin.akers5@gmail.com>
2019-12-08 01:06:12 -0600

    pushing base pages, routes,and navbar

--- real-world-nuxt/layouts/default.vue
+++ real-world-nuxt/layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
+    <nav-bar />
     <nuxt />
   </div>
 </template>
+
+<script>
+import NavBar from '~/components/NavBar.vue';
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
 
  <style>
     html {
