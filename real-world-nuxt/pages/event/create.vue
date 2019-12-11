b9f6886176819b9592c81a58a42e59fce174b102
unknown <austin.akers5@gmail.com>
2019-12-10 23:57:57 -0600

    created dynamic title

---
+++ real-world-nuxt/pages/event/create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Create an Events</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create an event in your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
