d5c82df3c2f26cf6917c76e366b817c4bba83f4e
unknown <austin.akers5@gmail.com>
2020-02-15 11:21:46 -0600

    pushing registration page

--- vue-auth/server.js
+++ vue-auth/server.js
@@ -16,9 +16,15 @@ app.get('/', (req, res) => {
   })
 })
 
-app.get('/dashboard', (req, res) => {
-  res.json({
-    events: events
+app.get('/dashboard', verifyToken, (req, res) => { //verifyToken is middleware
+  jwt.verify(req.token, 'the_secret_key', err => { // verifies token
+    if (err) { // if error, respond with 401 code
+      res.sendStatus(401)
+    } else { // otherwise, respond with private data
+      res.json({
+        events: events
+      })
+    }
   })
 })
 
--- vue-auth/src/router.js
+++ vue-auth/src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Dashboard from './views/Dashboard.vue'
-
+import RegisterUser from './views/RegisterUser.vue'
 Vue.use(Router)
 
 const router = new Router({
@@ -18,6 +18,11 @@ const router = new Router({
       path: '/dashboard',
       name: 'dashboard',
       component: Dashboard
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterUser
     }
   ]
 })
