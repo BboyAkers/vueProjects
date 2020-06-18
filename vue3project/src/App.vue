fec5087a59d39c0e823fe95c5e05457f6311d98f
unknown <austin.akers5@gmail.com>
2020-06-17 23:40:44 -0500

    pushing vue 3 capacity component using composition api

---
+++ vue3project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <CapacityComponent />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import CapacityComponent from './components/CapacityComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    CapacityComponent,
+  },
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3project/src/components/CapacityComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>Spaces Left: {{ spacesLeft }} out of {{ capacity }}</p>
+    <button @click="increaseCapacity()">Increase Capacity</button>
+    <ul>
+      <li v-for="(name, index) in attending" :key="index">
+        {{ name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+export default {
+  setup() {
+    const capacity = ref(3)
+    const attending = ref(['Tim', 'Bob', 'Joe'])
+    const spacesLeft = computed(() => {
+      return capacity.value - attending.value.length
+    })
+
+    function increaseCapacity() {
+      capacity.value++
+    }
+    return { capacity, attending, spacesLeft, increaseCapacity }
+  },
+}
+</script>
