a8d039bfa1f4c19a9b40a8a23924ecdfc38ea97e
Austin Akers <austin.akers5@gmail.com>
2022-07-19 23:51:59 -0700

    created base for forum app

---
+++ forumapp/src/components/PageHome.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div v-for="thread in threads" :key="thread.id">
+      <h2>{{thread.title}}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+        <h2>{{findUserById(findPostById(postId).userId).name}}</h2>
+        <h2>{{findPostById(postId).text}}</h2>
+      </div>
+    </div>
+    
+    <div v-for="user in users" :key="user.id">
+      <h2>{{user.name}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  name: 'PageHome',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      threads : sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    findPostById (postId) {
+      return this.posts.find(p => p.id === postId)
+    },
+    findUserById (userId) {
+      return this.users.find(u => u.id === userId)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
