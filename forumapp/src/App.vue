af0e5d280e87d5719fad023460f83f95421bda2c
Austin Akers <austin.akers5@gmail.com>
2023-07-26 15:11:37 -0700

    pushing forum app

---
+++ forumapp/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="container">
+    <page-home/>
+  </div>
+</template>
+
+<script>
+import PageHome from '@/components/PageHome'
+export default {
+  name: 'App',
+  components: {
+    PageHome
+  }
+}
+</script>
+
+<style>
+@import "assets/style.css";
+</style>
